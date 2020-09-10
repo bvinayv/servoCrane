5ac10fc722bd09cf1325c4e9b1f74ef8dc9bdfa4
wenywrma <46212708+bvinayv@users.noreply.github.com>
2020-09-10 09:16:14 +0530

    Initial commit

---
+++ ProjectCrane_Master/ProjectCrane_Master.ino
@@ -0,0 +1,91 @@
+// MASTER FUNCTION CODE 
+
+/* FOR 3 JOTSTICKS Which passes 'a' - 'e' and '1' - '6' charcters 
+   to the SLAVE bluetooth module */
+
+int joy1X = A0;
+int joy1Y = A1;
+int joy2X = A2;
+int joy2Y = A3;
+int joy3X = A4;
+int joy3Y = A5;
+int val1 , val2 , val3 , val4 , val5 , val6;
+int nval1 , nval2 , nval3 , nval4 , nval5 , nval6;
+   
+void setup() {
+ pinMode(joy1X,INPUT);  // motor for left and right
+ pinMode(joy1Y,INPUT);  // motor for front and back
+ pinMode(joy2X,INPUT);  // servo for base
+ pinMode(joy2Y,INPUT);  // servo for HAND primary
+ pinMode(joy3X,INPUT);  // picking the objects
+ pinMode(joy3Y,INPUT);  // servo for HAND secondary
+ Serial.begin(38400);
+}
+
+void loop() {
+// motor for front and back
+  val1 = analogRead(joy1X);
+  nval1 = map( val1 , 0 , 1023 , 0 , 180 );
+  //Serial.print(nval1);
+  if ( nval1 > 110 )
+  Serial.print('a');
+  else if ( nval1 < 80 )
+  Serial.print('1');
+  else if ( nval1 > 80 && nval1 < 110 )
+  Serial.print('s');
+  
+  
+// motor for left and right
+  val2 = analogRead(joy1Y);
+  nval2 = map( val2 , 0 , 1023 , 0 , 180 );
+  //Serial.print(nval2);
+  if ( nval2 > 110 )
+  Serial.print('b');
+  else if ( nval2 < 80 )
+  Serial.print('2');
+
+// servo for base
+  val3 = analogRead(joy2X);
+  nval3 = map( val3 , 0 , 1023 , 0 , 180 );
+  //Serial.print(nval3);
+  if ( nval3 > 110 )
+  Serial.print('c');
+  else if ( nval3 < 80 )
+  Serial.print('3');
+
+// servo for HAND primary
+  val4 = analogRead(joy2Y);
+  nval4 = map( val4 , 0 , 1023 , 0 , 180 );
+  //Serial.print(nval4);
+  if ( nval4 > 110 )
+  Serial.print('d');
+  else if ( nval4 < 80 )
+  Serial.print('4');
+
+// picking the objects
+ val5 = analogRead(joy3X);
+  nval5 = map( val5 , 0 , 1023 , 0 , 180 );
+  //Serial.print(nval5);
+  if ( nval5 > 110 )
+  Serial.print('e');
+  else if ( nval5 < 80 )
+  Serial.print('5');
+
+// servo for HAND secondary4
+  val6 = analogRead(joy3Y);
+  nval6 = map( val6 , 0 , 1023 , 0 , 180 );
+  //Serial.print(nval6);
+  if ( nval6 > 110 )
+  Serial.print('f');
+  else if ( nval6 < 80 )
+  Serial.print('6');
+  delay(1000);
+  //Serial.print('z');
+// To clear the queue using the switch in the joystick
+  
+}
+
+
+
+
+
---
+++ ProjectCrane_Slave/ProjectCrane_Slave.ino
@@ -0,0 +1,152 @@
+// SLAVE FUNCTION CODE
+//For the servos and the motor controller
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+int m1_red = 2;
+int m1_black = 4;
+int m2_red = 7;
+int m2_black = 8;
+
+int pos;
+char state;
+int current_pos1;
+int current_pos2;
+int current_pos3;
+int current_pos4;
+
+void setup() {
+  servo1.attach(3);
+  servo2.attach(5);
+  servo3.attach(6);
+  servo4.attach(9);
+  pinMode( m1_red , OUTPUT );
+  pinMode( m1_black , OUTPUT );
+  pinMode( m2_red , OUTPUT );
+  pinMode( m2_black , OUTPUT );
+  Serial.begin(38400);
+}
+
+void loop() {
+  if (Serial.available() > 0){
+    state = Serial.read();
+  }
+  switch (state){
+  case 'a' :
+      // motor for front
+            digitalWrite(m1_red , HIGH );   
+            digitalWrite(m1_black , LOW );
+            digitalWrite(m2_black , LOW );
+            digitalWrite(m2_red , HIGH );
+            break;
+  case '1' :
+     // motor for back
+            digitalWrite(m1_red , LOW );   
+            digitalWrite(m1_black , HIGH );
+            digitalWrite(m2_black , HIGH );
+            digitalWrite(m2_red , LOW );
+            
+            break;
+  case 'b' :
+     // motor for left
+            digitalWrite(m2_black , LOW );
+            digitalWrite(m2_red , HIGH );
+            break;
+  case '2' :
+     // motor for right
+            digitalWrite(m1_red , HIGH );   
+            digitalWrite(m1_black , LOW );
+            break;
+     //perfect till above       
+  case 'c' :
+            current_pos1=servo1.read();
+            Serial.println(current_pos1);
+            if(current_pos1<170)
+            servo1.write(current_pos1 + 10);
+            else
+            servo1.write(180);
+            delay(1000);
+            break;
+  case '3' :
+            current_pos1=servo1.read();
+            Serial.println(current_pos1);
+            if(current_pos1>10)
+            servo1.write(current_pos1 - 10);
+            else
+            servo1.write(0);
+            delay(1000);
+            break;                              
+  case 'd' :
+            current_pos2=servo2.read();
+            Serial.println(current_pos2);
+            if(current_pos2<170)
+            servo2.write(current_pos2 + 10);
+            else
+            servo2.write(180);
+            delay(1000);
+            break;
+  case '4' :
+            current_pos2=servo2.read();
+            Serial.println(current_pos2);
+            if(current_pos2>10)
+            servo2.write(current_pos2 - 10);
+            else
+            servo2.write(0);
+            delay(1000);
+            break;
+            /* for moving the secondary arm */ 
+  case 'e' :
+            current_pos3=servo3.read();
+            Serial.println(current_pos3);
+            if(current_pos3<170)
+            servo3.write(current_pos3 + 10);
+            else
+            servo3.write(180);
+            delay(1000);
+            break;
+  case '5' :
+            current_pos3=servo3.read();
+            Serial.println(current_pos3);
+            if(current_pos3>10)
+            servo3.write(current_pos3 - 10);
+            else
+            servo3.write(0);
+            delay(1000);
+            break;
+            /* for picking the object */  
+  case 'f' :
+            current_pos4=servo4.read();
+            Serial.println(current_pos4);
+            if(current_pos4<170)
+            servo4.write(current_pos4 + 10);
+            else
+            servo4.write(180);
+            delay(1000);
+            break;
+  case '6' :
+            current_pos4=servo4.read();
+            Serial.println(current_pos4);
+            if(current_pos4>10)
+            servo4.write(current_pos4 - 10);
+            else
+            servo4.write(0);
+            delay(1000);
+            break;
+  case 's' : digitalWrite(m1_red , LOW );   
+            digitalWrite(m1_black , LOW );
+            digitalWrite(m2_black , LOW );
+            digitalWrite(m2_red , LOW ); 
+            break;
+ defalut :  current_pos1=servo1.read();
+            servo1.write(current_pos1);
+            current_pos2=servo2.read();
+            servo2.write(current_pos2);
+            current_pos3=servo3.read();
+            servo3.write(current_pos3);
+            current_pos4=servo4.read();
+            servo4.write(current_pos4);
+            } 
+}
